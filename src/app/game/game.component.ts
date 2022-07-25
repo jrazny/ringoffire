839c7aadb76217d02636649f73b986281f8fa7e9
Jan Razny <netorg@gmx.de>
2022-07-25 08:36:15 +0200

    250722

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -23,9 +23,17 @@ export class GameComponent implements OnInit {
   }
 
   takeCard() {
-    this.currentCard = this.game.stack.pop();
-    console.log('this.currentCard');
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      
+      console.log('New card: ' + this.currentCard);
+      console.log('Game is: ', this.game);
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 
 }
