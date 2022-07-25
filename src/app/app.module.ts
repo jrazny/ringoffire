8999e4afc1af9bfc78c52bf5b0b3341ca63252bd
Jan Razny <netorg@gmx.de>
2022-07-26 00:56:53 +0200

    dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,25 @@ import { StartscreenComponent } from './startscreen/startscreen.component';
 import { GameComponent } from './game/game.component';
 import { PlayerComponent } from './player/player.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartscreenComponent,
     GameComponent,
-    PlayerComponent
+    PlayerComponent,
+    DialogAddPlayerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
     BrowserAnimationsModule
   ],
   providers: [],
