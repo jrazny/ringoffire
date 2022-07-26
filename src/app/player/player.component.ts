a7116eb8cf4ed7ebcee07345bc038f0594e62852
Jan Razny <netorg@gmx.de>
2022-07-26 23:56:35 +0200

    next Player

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -7,7 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
 
-  @Input() name;
+  @Input() name: string;
+  @Input() playerActive: boolean = false;
 
   constructor() { }
 
