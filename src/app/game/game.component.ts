fb07a3f928d49c47a96352a9846a3510c0942c68
Jan Razny <netorg@gmx.de>
2022-07-28 00:32:56 +0200

    connect with firebase

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-game',
@@ -14,10 +15,17 @@ export class GameComponent implements OnInit {
   game: Game;
   dialogRef: any;
   
-  constructor(public dialog: MatDialog) { }
+  constructor(private firestore: AngularFirestore, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.newGame();
+    this
+      .firestore
+      .collection('games')
+      .valueChanges()
+      .subscribe((game) => {
+        console.log('Game update', game);
+      });
   }
 
   newGame() {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-a5714',
+    appId: '1:403506513841:web:b5becf08b35ac7474cdf90',
+    storageBucket: 'ring-of-fire-a5714.appspot.com',
+    apiKey: 'AIzaSyBkLd4lWbExuclqwf9QZF4IG9pECp52XZg',
+    authDomain: 'ring-of-fire-a5714.firebaseapp.com',
+    messagingSenderId: '403506513841',
+  },
   production: false
 };
 
